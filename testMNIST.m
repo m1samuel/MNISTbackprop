4e193f86e96930bc90e0a09a67737e5c96536254
Malcolm Samuel <m1samuel@ucsd.edu>
2015-03-02 23:07:22 -0800

    updated test to show what our net thought digits were

--- testMNIST.m
+++ testMNIST.m
@@ -3,15 +3,19 @@ function e = testMNIST(images,labels,W_in,W_out)
     In_Out = [In_Out ones(length(In_Out),1) labels];
     ec = 0;
     f = @(u)exp(u)./sum(exp(u));
-    misclassified = zeros(10,1);
+    misclassified = zeros(10,11);
+    % misclassified is a matrix where the first column is
+    % what the digit really was and the corresponding rows
+    % to each element in the column is what our net though it was
     for i=1:length(images)
         %compute Hidden unit values
         H = f([In_Out(i,1:end-1) * W_in(1:end,:) 1]');
         %compute output
         Output = f((W_out*H));
         [~,j] = max(Output);
-        misclassified(In_Out(i,end)+1) = misclassified(In_Out(i,end)+1) +1;
         if (~isequal(j,In_Out(i,end)+1))
+            misclassified(In_Out(i,end)+1,1) = misclassified(In_Out(i,end)+1)+1;
+            misclassified(In_Out(i,end)+1,j+1) = misclassified(In_Out(i,end)+1,j+1)+1;
             ec = ec + 1;
         end
     end
